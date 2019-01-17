1aa916ae05e0753f78a108ed290bdb31befca6d8
ProMoFu <promofu@online.de>
2019-01-18 00:18:45 +0100

    updated

--- serve_it.sh
+++ serve_it.sh
@@ -9,6 +9,7 @@ docker run --rm \
   --volume "$PWD:/srv/jekyll" \
   --volume "$PWD/_bundle:/usr/local/bundle" \
   --publish "4000:4000" \
+  --publish "35729:35729" \
   --env "JEKYLL_ENV=devel" \
   -it jekyll/jekyll:$JEKYLL_VERSION \
   jekyll serve -w -l -V --config _config.yml,_config.dev.yml --future

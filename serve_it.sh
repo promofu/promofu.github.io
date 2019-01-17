7e6afa0a1f6a783764d3d522b5ec1f35d8cbefbc
ProMoFu <promofu@online.de>
2019-01-17 21:16:21 +0100

    basing on academicpages theme

---
+++ serve_it.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# for more info read:
+# https://github.com/envygeeks/jekyll-docker/blob/master/README.md
+
+export JEKYLL_VERSION=latest
+
+docker run --rm \
+  --volume "$PWD:/srv/jekyll" \
+  --volume "$PWD/_bundle:/usr/local/bundle" \
+  --publish "4000:4000" \
+  --env "JEKYLL_ENV=devel" \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  jekyll serve -w -l -V --config _config.yml,_config.dev.yml --future
